1c79dcb825b12e5e2952f05c84acab3dd56d1b46
Toon <toon.borgers@cegeka.be>
2015-02-16 11:03:29 +0100

    keystores

--- app/src/androidTest/java/be/cegeka/androidcd/DummyTest.java
+++ app/src/androidTest/java/be/cegeka/androidcd/DummyTest.java
@@ -1,8 +1,10 @@
 package be.cegeka.androidcd;
 
+import android.test.AndroidTestCase;
+
 import junit.framework.Assert;
 
-public class DummyTest {
+public class DummyTest extends AndroidTestCase {
     public void testBlaBla(){
         Assert.assertEquals(true,true);
     }
