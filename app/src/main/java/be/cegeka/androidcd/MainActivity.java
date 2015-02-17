bb8e26bc7c45b8ccc31a01b4b0e245d6ab6722ab
Toon <toon.borgers@cegeka.be>
2015-02-17 15:54:05 +0100

    crashlytics

--- app/src/main/java/be/cegeka/androidcd/MainActivity.java
+++ app/src/main/java/be/cegeka/androidcd/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Crashlytics.start(this);
         setContentView(R.layout.activity_main);
     }
 }
