1c532498f2f26f754d3dff669611fe4fe66fed0c
Toon <toon.borgers@cegeka.be>
2015-02-17 08:24:10 +0100

    menu weg

--- app/src/main/java/be/cegeka/androidcd/MainActivity.java
+++ app/src/main/java/be/cegeka/androidcd/MainActivity.java
@@ -13,24 +13,4 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
